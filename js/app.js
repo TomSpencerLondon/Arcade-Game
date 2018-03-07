b9d7d6ba5bedb149180bfdffc94891201be5b342
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-03-07 21:31:36 +0000

    Add modal for display and Score and Level

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= 505) {
         this.x = 0;
     }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,12 +46,31 @@ Player.prototype.update = function() {
     // If the player makes it to the water, reset their position
     if (this.y < 0) {
         this.y = 300;        
-        allEnemies.push(new Enemy());  // Add an extra enemy
+        score += 1;
+        gameLevel += 1;
+        allEnemies.push(new Enemy(0, Math.random() * 184 + 50, Math.random() * 250));  // Add an extra enemy
     }
+    allEnemies.forEach((enemy)=> {
+        this.checkCollision(enemy)
+    });
 };
+
+// Function to display player's score
+var displayScoreLevel = function(Score, Level) {
+    var canvas = document.getElementsByTagName('canvas');
+    var firstCanvasTag = canvas[0];
+
+    // add player score and level to div element created
+    scoreLevelDiv.innerHTML = 'Score: ' + Score
+        + ' / ' + 'Level: ' + Level;
+    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
+};
+
+
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    displayScoreLevel(score, gameLevel);
 };
 
 Player.prototype.handleInput = function(keyPress) {
@@ -66,7 +86,34 @@ Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'down') {
         player.y += player.speed - 20;
     }
-    console.log('keyPress is: ' + keyPress);
+    // console.log('keyPress is: ' + keyPress);
+    console.log('player position is: ' + 'x =' + this.x + ' y =' + this.y)
+};
+
+Player.prototype.checkCollision = function(anEnemy) {
+
+    if (
+        player.y + 131 >= anEnemy.y + 90
+        && player.x + 25 <= anEnemy.x + 88
+        && player.y + 73 <= anEnemy.y + 135
+        && player.x + 76 >= anEnemy.x + 11) {
+        openModal();
+        player.x = 202.5;
+        player.y = 383;
+        score = 0;
+        gameLevel = 1;
+    }
+
+if (player.y > 420) {
+    player.y = 420;
+}
+if (player.x > 400) {
+    player.x = 400;
+}
+if (player.x < 2.5) {
+    player.x = 2.5;
+}  
+
 };
 
 
@@ -75,6 +122,9 @@ Player.prototype.handleInput = function(keyPress) {
 // Place the player object in a variable called player
 var allEnemies = [];
 var player = new Player(250, 300, 50);
+var score = 0;
+var gameLevel = 1;
+var scoreLevelDiv = document.createElement('div');
 // var enemy = new Enemy(0, Math.random() * )
 var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 250);
 allEnemies.push(enemy);
@@ -91,3 +141,34 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//Get modal element 
+var modal = document.getElementById('simpleModal'); 
+
+//Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
+
+//Listen for close modal cross 
+closeBtn.addEventListener('click', closeModal); 
+//Listen for outside click
+window.addEventListener('click', clickOutside);
+
+//Function to open modal 
+function openModal(){
+    modal.style.display = 'block'; 
+} 
+
+//Function to close modal 
+function closeModal(){
+    modal.style.display = 'none'; 
+} 
+
+//Function to clickOutside modal and close modal
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none'; 
+    }
+
+} 
